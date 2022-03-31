efb1551591a40e49aba31109431f54e9d89a2ccc
Anxin Canada <75194970+AnxinCanada@users.noreply.github.com>
2022-03-31 10:05:12 -0700

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,2 +1,2 @@
 import os
-os.system(f"python3 sussy.py {input("URL: ")} -p {input("Port:")} -ua -v -s 10000 --sleeptime 0")
+os.system(f'python3 sussy.py {input("URL: ")} -p {input("Port:")} -ua -v -s 10000 --sleeptime 0')
